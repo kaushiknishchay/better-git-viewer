dc87fa868ef24b7c7ca2dbc8d9959ec36539cdd6
Nishchay Kaushik <kaushiknishchay@gmail.com>
2020-12-13 22:56:02 +0530

    feat: add form to add repo url with progress

--- git_options.js
+++ git_options.js
@@ -1,10 +1,42 @@
+import { clone } from 'isomorphic-git';
 import http from 'isomorphic-git/http/web';
 import LightningFS from '@isomorphic-git/lightning-fs';
-const fs = new LightningFS('fs');
 
-export const defaultGitOptions = {
+const DB_NAME = 'fs';
+const REPO_DIR = '/repo';
+const defaultGitOptions = {
   fs,
   http,
-  dir: '/repo',
+  dir: REPO_DIR,
   corsProxy: 'https://cors.isomorphic-git.org',
 };
+
+const fs = new LightningFS(DB_NAME, {
+  wipe: true,
+});
+
+function flushDB() {
+  return new Promise((resolve, reject) => {
+    const DBDeleteRequest = window.indexedDB.deleteDatabase(DB_NAME);
+
+    DBDeleteRequest.onerror = function (event) {
+      reject(event);
+    };
+
+    DBDeleteRequest.onsuccess = function (event) {
+      resolve(event);
+    };
+  });
+}
+
+function cloneRepo(url, repoBranch = 'master', onProgress, onMessage) {
+  return clone({
+    ...defaultGitOptions,
+    url,
+    ref: repoBranch,
+    onMessage,
+    onProgress,
+  });
+}
+
+export { fs, defaultGitOptions, REPO_DIR, cloneRepo, flushDB };
